d0bbd3157060f5904f9e59e6ee50f6d4dffef4c7
Robert Wayne <robertjwayne@gmail.com>
2017-05-31 16:04:22 -0400

    Create show page for appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,16 @@
 class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
+
+  def patient_name
+    self.patient.name
+  end
+
+  def doctor_name
+    self.doctor.name
+  end
+
+  def readable_datetime
+    self.appointment_datetime.strftime("%B %-d, %Y at %R")
+  end
 end
